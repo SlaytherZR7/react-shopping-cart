f427e2d8724b28b0271dc9da6b3e9e8e680a6903
SlaytherZR7 <slayther.zamora@epn.edu.ec>
2025-02-26 18:34:21 -0500

    refactor App component to integrate CartProvider, Header, and Cart components for improved structure and functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
-import { Products } from './components/Products'
-import { products } from './mocks/products.json'
+import { products as initialProducts } from './mocks/products.json'
+import { Products } from './components/Products.jsx'
+import { Header } from './components/Header.jsx'
+import { useFilters } from './hooks/useFilter.js'
+import { Cart } from './components/Cart.jsx'
+import { CartProvider } from './context/cart.jsx'
 
-const App = () => {
+function App () {
+  const { filterProducts } = useFilters()
+
+  const filteredProducts = filterProducts(initialProducts)
+
   return (
-    <Products products={products} />
+    <CartProvider>
+      <Header />
+      <Cart />
+      <Products products={filteredProducts} />
+    </CartProvider>
   )
 }
 
